7c2a72ee69c2b38f1f3d4c75cdd2d9d18ae8011f
Rafael Sanchez <paocomcarnemoidaetubaina@gmail.com>
2026-07-12 19:57:18 -0300

    Spring bootstrap

---
+++ telelogger/src/main/java/com/rafaiska/telelogger/TeleloggerApplication.java
@@ -0,0 +1,13 @@
+package com.rafaiska.telelogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleloggerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TeleloggerApplication.class, args);
+	}
+
+}
---
+++ telelogger/src/test/java/com/rafaiska/telelogger/TeleloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaiska.telelogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeleloggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
